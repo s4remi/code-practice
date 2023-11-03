3643a79cf0996cef3c66a7a3064021ae4d351459
s4remi <s4remi@gmail.com>
2023-11-03 13:46:31 -0700

    setting the html attributes dynamically &loding image files

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,11 +1,18 @@
+import reactImg from "./assets/react-core-concepts.png";
+
+const reactDescription = ["Fundamental", "Crucial", "Core"];
+function randomIntGen(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
-        going to build!
+        {reactDescription[randomIntGen()]} React concepts you will need for
+        almost any app you are going to build!
       </p>
     </header>
   );
