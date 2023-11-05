28d9fc4f11cd5e2c3e806c369a9ba7c5df84a1f8
s4remi <s4remi@gmail.com>
2023-11-05 14:00:25 -0800

    make folder for each component if it is neccesarry

---
+++ 01-starting-project/src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import reactImg from "../../assets/react-core-concepts.png";
+import "./Header.css";
+
+const reactDescription = ["Fundamental", "Crucial", "Core"];
+function randomIntGen(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+export default function Header() {
+  return (
+    <header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {reactDescription[randomIntGen(2)]} React concepts you will need for
+        almost any app you are going to build!
+      </p>
+    </header>
+  );
+}
