337459746ae4022a032c3565bcc86bc01d42015d
s4remi <s4remi@gmail.com>
2023-11-05 14:40:06 -0800

    add other section to the homepage as a button without giving ant addeventlistener to buttin

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -2,9 +2,9 @@ import CoreConceptImg from "./assets/components.png";
 import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/tabButton.jsx";
 
 // nested components,
-
 function App() {
   return (
     <div>
@@ -27,7 +27,12 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
-        <h3>next time </h3>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+          </menu>
+        </section>
       </main>
     </div>
   );
