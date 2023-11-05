2cf3aa88a7925d4b3291f32f17614371dad62769
s4remi <s4remi@gmail.com>
2023-11-05 15:03:37 -0800

    reacting to events

--- 01-starting-project/src/components/TabButton.jsx
+++ 01-starting-project/src/components/TabButton.jsx
@@ -1,7 +1,12 @@
 export default function TabButton(props) {
+  // add event listener
+  function clickHandle() {
+    console.log("hello world!");
+  }
+
   return (
     <li>
-      <button>{props.children}</button>
+      <button onClick={clickHandle}>{props.children}</button>
     </li>
   );
 }
