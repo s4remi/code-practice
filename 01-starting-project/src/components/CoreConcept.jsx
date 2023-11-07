10762886003fa23181f5117d6b94341d5fd5c670
s4remi <s4remi@gmail.com>
2023-11-07 10:31:21 -0800

    Remove untracked files

--- 01-starting-project/src/components/CoreConcept.jsx
+++
@@ -1,9 +0,0 @@
-export default function CoreConcept(props) {
-  return (
-    <li>
-      <img src={props.image} alt={props.description} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </li>
-  );
-}
--- 01-starting-project/src/components/TabButton.jsx
+++
@@ -1,8 +0,0 @@
-export default function TabButton({ children, onSelect }) {
-  // add event listener
-  return (
-    <li>
-      <button onClick={onSelect}>{children}</button>
-    </li>
-  );
-}
