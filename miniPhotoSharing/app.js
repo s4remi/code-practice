5903f4aaacc11e10e5197d0910db50c4af57ef64
s4remi <s4remi@gmail.com>
2023-11-08 23:06:30 -0800

    adding react as front end using http-server

--- miniPhotoSharing/app.js
+++ miniPhotoSharing/app.js
@@ -19,8 +19,9 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "front", "dist")));
 
 app.use("/", indexRouter);
+//console.log("hello");
 
 export default app;
---
+++ miniPhotoSharing/front/src/App.jsx
@@ -0,0 +1,8 @@
+export default function Greeting() {
+  return (
+    <div>
+      <h1>Hello World! how you doing</h1>
+      <p>meoo meoo</p>
+    </div>
+  );
+}
