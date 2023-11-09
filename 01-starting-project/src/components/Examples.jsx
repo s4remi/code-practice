57c918f68015978009b1b45742ee70e0de895255
s4remi <s4remi@gmail.com>
2023-11-08 17:28:04 -0800

    setting components types dynamically

--- 01-starting-project/src/components/Examples.jsx
+++ 01-starting-project/src/components/Examples.jsx
@@ -27,6 +27,7 @@ export default function Examples() {
   return (
     <Section title="Examples" id="examples">
       <Tabs
+        buttonsContainer="menu"
         buttons={
           <>
             <TabButton
