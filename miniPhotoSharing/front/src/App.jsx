ec9aab33f42f15066ef729d56b7f0fcebafb7a8a
s4remi <s4remi@gmail.com>
2023-11-09 07:58:39 -0800

    connect front and back-end server with each other

--- miniPhotoSharing/front/src/App.jsx
+++ miniPhotoSharing/front/src/App.jsx
@@ -1,8 +1,21 @@
-export default function Greeting() {
+export default function App() {
+  //basic test to see if we can see the data from the back
+  async function testBack() {
+    console.log("Testing back ....");
+    const response = await fetch("/api/data");
+    const data = await response.json();
+    console.log("Got data!", data);
+  }
+  testBack();
   return (
     <div>
-      <h1>Hello World! how you doing</h1>
-      <p>meoo meoo</p>
+      <h1>\tHello! World! </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis
+        pariatur tenetur impedit exercitationem, excepturi sint dolores nulla
+        voluptas provident, iusto nihil et ullam cumque amet voluptates fugit
+        eio!
+      </p>
     </div>
   );
 }
