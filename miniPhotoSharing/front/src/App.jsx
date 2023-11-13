2a12885e5f0ead0d0debdee2996e74dd0b7b962c
s4remi <s4remi@gmail.com>
2023-11-13 15:47:51 -0800

    add button with counting how many times was clicked

--- miniPhotoSharing/front/src/App.jsx
+++ miniPhotoSharing/front/src/App.jsx
@@ -1,6 +1,56 @@
 import { AppNavBar } from "./layout/AppNavBar";
 import { AppFooter } from "./layout/AppFooter";
 
+function PhotosGallery(props) {
+  function renderPhoto(d, i) {
+    // i is index in here, using it as key for map
+    return (
+      <div key={`phot_${i}`}>
+        <img src={d.url} alt={d.caption} />
+        <div>{d.caption}</div>
+      </div>
+    );
+  }
+  function renderPhotos() {
+    return <div>{props.photos.map(renderPhoto)}</div>;
+  }
+  console.log("Render photos Gallery props= ", props);
+  return (
+    <div className="photoGallery">
+      {" "}
+      <h2>PHOTOS</h2>
+      <div>{renderPhotos()}</div>
+    </div>
+  );
+}
+import PropTypes from "prop-types";
+import { useState } from "react";
+
+function ButtonVote({ name }) {
+  //let votes = 0;
+  let [votes, setVote] = useState(0);
+
+  function onClick() {
+    setVote(votes + 1);
+    //votes += 1;
+    console.log(`Voted for ${name} votes=${votes}`);
+  }
+  console.log("render buttonVote", name, votes);
+  return (
+    <div>
+      <button className="btn btn-primary mb-2" onClick={onClick}>
+        vote for {name}
+      </button>
+      <output>
+        {name} has {votes} votes
+      </output>
+    </div>
+  );
+}
+ButtonVote.propTypes = {
+  name: PropTypes.string.isRequired,
+};
+
 export default function App() {
   //basic test to see if we can see the data from the back
   async function testBack() {
@@ -10,18 +60,19 @@ export default function App() {
     console.log("Got data!", data);
   }
   testBack();
-  const photos = [{ url: "", caption: "" }];
-  function renderPhotos() {
-    return <div>{photos.map()}</div>;
-  }
+  // we can have default variables
+  const phot = [
+    { url: "test", caption: "cap1" },
+    { url: "test", caption: "cap2" },
+  ];
   return (
     <div>
       <AppNavBar />
       <h1>Photo Sharing</h1>
-      <h2>Photos</h2>
-      {renderPhotos()}
+      <PhotosGallery photos={phot} />
+      <ButtonVote name="ali" />
+      <ButtonVote name="john" />
       <AppFooter />
     </div>
   );
 }
-//
