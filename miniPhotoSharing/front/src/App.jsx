a5472fc7362270e073b1ceb35a42b101491bf9be
s4remi <s4remi@gmail.com>
2023-11-13 16:47:06 -0800

    add state for photo gallery and showing the photos of items between 30 to 47

--- miniPhotoSharing/front/src/App.jsx
+++ miniPhotoSharing/front/src/App.jsx
@@ -2,26 +2,36 @@ import { AppNavBar } from "./layout/AppNavBar";
 import { AppFooter } from "./layout/AppFooter";
 import { ButtonVote } from "./components/ButtonVote";
 import { PhotosGallery } from "./components/PhotosGallery";
+import { useState, useEffect } from "react";
 
 export default function App() {
+  let [photos, setPhotos] = useState([]);
   //basic test to see if we can see the data from the back
   async function testBack() {
     console.log("Testing back ....");
     const response = await fetch("/api/photos");
     const data = await response.json();
     console.log("Got data!", data);
+    setPhotos(data);
   }
-  testBack();
+  useEffect(() => {
+    testBack();
+    return () => {
+      //clean up for the effect
+    };
+  }, []); //when we pass the empty array it means it run once
+
   // we can have default variables
-  const photos = [
-    { url: "test", caption: "cap1" },
-    { url: "test", caption: "cap2" },
-  ];
+  // photos = [
+  //   { url: "test", caption: "cap1" },
+  //   { url: "test", caption: "cap2" },
+  // ];
+  console.log("Render App photos=", photos);
   return (
     <div>
       <AppNavBar />
       <h1>Photo Sharing</h1>
-      <PhotosGallery photos={photos} />
+      <PhotosGallery photos={photos.slice(30, 47)} />
       <ButtonVote name="ali" />
       <ButtonVote name="john" />
       <AppFooter />
