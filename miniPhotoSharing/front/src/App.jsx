869c74b08ea56b9a76c4ba5e9cdd52f56e0e1ebc
s4remi <s4remi@gmail.com>
2023-11-13 17:47:31 -0800

    cleanup

--- miniPhotoSharing/front/src/App.jsx
+++ miniPhotoSharing/front/src/App.jsx
@@ -4,6 +4,47 @@ import { ButtonVote } from "./components/ButtonVote";
 import { PhotosGallery } from "./components/PhotosGallery";
 import { useState, useEffect } from "react";
 
+function SearchBar() {
+  let [query, setQuery] = useState("");
+
+  function onInput(evt) {
+    console.log("SearchBar OnInput", evt.target.value);
+    setQuery(evt.target.value);
+  }
+  return (
+    <div>
+      Search{" "}
+      <input
+        className="input-control"
+        type="text"
+        onInput={onInput}
+        value={query}
+      />
+    </div>
+  );
+}
+
+function RangeWidth() {
+  let [width, setWidth] = useState(10);
+  function onInput(evt) {
+    setWidth(+evt.target.value);
+  }
+  return (
+    <>
+      <label>
+        <input
+          type="range"
+          min="40"
+          max="300"
+          value={width}
+          onInput={onInput}
+        />
+        <output>value {width}</output>
+      </label>
+    </>
+  );
+}
+
 export default function App() {
   let [photos, setPhotos] = useState([]);
   //basic test to see if we can see the data from the back
@@ -31,6 +72,9 @@ export default function App() {
     <div>
       <AppNavBar />
       <h1>Photo Sharing</h1>
+
+      <RangeWidth width={10} />
+      <SearchBar />
       <PhotosGallery photos={photos.slice(30, 47)} />
       <ButtonVote name="ali" />
       <ButtonVote name="john" />
