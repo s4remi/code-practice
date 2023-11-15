fd1b702b36db41aef3228851edcb6a6d28aa12a1
s4remi <s4remi@gmail.com>
2023-11-15 13:22:41 -0800

    showing the data from db in the front

--- miniPhotoSharing/front/src/components/PhotosGallery.jsx
+++ miniPhotoSharing/front/src/components/PhotosGallery.jsx
@@ -1,6 +1,37 @@
 import { PropTypes } from "prop-types";
 
 export function PhotosGallery({ photos }) {
+  console.log("Render PhotoGallery photos=", photos);
+
+  function renderPhotos() {
+    function renderPhoto(photo) {
+      return (
+        <div>
+          <div>{photo.url}</div>
+          <div>{photo._id}</div>
+          <div>{photo._caption}</div>
+          <div>{photo._owner}</div>
+        </div>
+      );
+    }
+    return photos.map(renderPhoto);
+  }
+  return <div className="row">{renderPhotos()}</div>;
+}
+
+PhotosGallery.propTypes = {
+  photos: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.string.isRequired,
+      url: PropTypes.string.isRequired,
+      owner: PropTypes.string.isRequired,
+      caption: PropTypes.string.isRequired,
+    })
+  ),
+};
+
+/*
+  console.log("Render photoGallery photos=", photos);
   function renderPhoto(d, i) {
     // i is index in here, using it as key for map
     return (
@@ -17,8 +48,7 @@ export function PhotosGallery({ photos }) {
   }
   console.log("Render photos Gallery photos= ", photos);
   return (
-    <div className="photoGallery">
-      {" "}
+    <div className="row">
       <h2>PHOTOS</h2>
       <div>{renderPhotos()}</div>
     </div>
@@ -35,3 +65,4 @@ PhotosGallery.propTypes = {
     })
   ).isRequired,
 };
+*/
